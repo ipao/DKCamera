88dd1e0ef15dfe8895b00218137b945fe05c0f77
kamonpop <1935925+ipao@users.noreply.github.com>
2024-06-26 15:26:09 +0700

    fix spm

--- DKCamera/DKCamera.swift
+++ DKCamera/DKCamera.swift
@@ -1150,9 +1150,12 @@ public extension CMAcceleration {
 public extension Bundle {
     
     class func cameraBundle() -> Bundle {
+#if SWIFT_PACKAGE
+        return Bundle.module
+#else
         let assetPath = Bundle(for: DKDefaultCameraResource.self).resourcePath!
         return Bundle(path: (assetPath as NSString).appendingPathComponent("DKCameraResource.bundle"))!
-//        return Bundle.module
+#endif
     }
     
 }
